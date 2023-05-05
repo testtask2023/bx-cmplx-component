089b49042d82b2227bc882592ba00ea3d489d34e
testtask2023 <nvsatr@gmail.com>
2023-05-05 18:29:51 +0500

    fix params

--- local/components/nspace/catalog/templates/.default/detail.php
+++ local/components/nspace/catalog/templates/.default/detail.php
@@ -16,8 +16,8 @@ $APPLICATION->IncludeComponent(
     "main",
     [
         "COMPONENT_TEMPLATE" => ".default",
-        "IBLOCK_TYPE" => "strategic_communications",
-        "IBLOCK_ID" => "5",
+        "IBLOCK_ID" => $arParams['IBLOCK_ID'],
+        "IBLOCK_TYPE" => $arParams['IBLOCK_TYPE'],
         "ELEMENT_ID" => "",
         "ELEMENT_CODE" => $component->variables['ELEMENT_CODE'],
         "SECTION_ID" => "",
